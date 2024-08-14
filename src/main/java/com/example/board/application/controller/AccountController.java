e5a0c558687e37a3dda3bd16ede5bd12f934ea46
김도현 <nanayagoon@daum.net>
2024-08-14 11:57:27 +0900

    Feat/#5: jwt 기반 로그인 구현

--- src/main/java/com/example/board/application/controller/AccountController.java
+++ src/main/java/com/example/board/application/controller/AccountController.java
@@ -2,12 +2,13 @@ package com.example.board.application.controller;
 
 
 import com.example.board.application.dto.request.CreateAccountRequest;
+import com.example.board.application.dto.request.LoginAccountRequest;
 import com.example.board.application.dto.response.CanUseMemberIdResponse;
 import com.example.board.application.facade.AccountFacade;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,7 +25,6 @@ public class AccountController {
         CanUseMemberIdResponse response = accountFacade.canUseMemberId(memberId);
         return ResponseEntity.ok(response);
     }
-
     @PostMapping("")
     public ResponseEntity<Void> createAccount(
             @RequestBody CreateAccountRequest createAccountRequest
@@ -33,5 +33,23 @@ public class AccountController {
         accountFacade.registerMember(createAccountRequest);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> loginAccount(@RequestBody LoginAccountRequest loginAccountRequest){
+        //accessToken 을 발급 해야함. jwt 에서.
+
+        String accessToken = accountFacade.loginAccount(
+                loginAccountRequest.getMemberId(),
+                loginAccountRequest.getPassword()
+        );
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Authorization","Bearer "+accessToken);
+
+        return ResponseEntity.ok().headers(httpHeaders).build();
+    }
+
+
+
 
 }
