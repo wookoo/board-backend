85588f96f982ad4e501e6e8c0b9191580e7dac31
김도현 <nanayagoon@daum.net>
2024-08-27 23:42:11 +0900

    Ref/#11: 작성자 정보 InnerClass 로 변경

--- src/main/java/com/example/board/application/dto/response/BoardDetailResponse.java
+++ src/main/java/com/example/board/application/dto/response/BoardDetailResponse.java
@@ -2,7 +2,10 @@ package com.example.board.application.dto.response;
 
 import com.example.board.domain.board.entity.Board;
 import com.example.board.domain.member.entity.Member;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDateTime;
 
@@ -13,17 +16,26 @@ public class BoardDetailResponse {
     private String content;
     private int view;
     private LocalDateTime createdAt;
+    private MemberInBoardDetailResponse member;
 
-    private String memberNickname;
-    private long memberId;
 
-    private BoardDetailResponse(String title, String content, int view, LocalDateTime createdAt, String memberNickname, long memberId) {
+    @Getter
+    @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+    private static class MemberInBoardDetailResponse {
+        private final long id;
+        private final String nickName;
+
+        public static MemberInBoardDetailResponse from(Member member) {
+            return new MemberInBoardDetailResponse(member.getId(), member.getNickName());
+        }
+    }
+
+    private BoardDetailResponse(String title, String content, int view, LocalDateTime createdAt, MemberInBoardDetailResponse member) {
         this.title = title;
         this.content = content;
         this.view = view;
         this.createdAt = createdAt;
-        this.memberNickname = memberNickname;
-        this.memberId = memberId;
+        this.member = member;
     }
 
     public static BoardDetailResponse from(Board board) {
@@ -32,8 +44,9 @@ public class BoardDetailResponse {
         int view = board.getView();
         LocalDateTime createdAt = board.getCreatedAt();
         Member member = board.getMember();
-        String nickname = member.getNickName();
-        long memberId = member.getId();
-        return new BoardDetailResponse(title, content, view, createdAt, nickname, memberId);
+        return new BoardDetailResponse(title, content, view, createdAt, MemberInBoardDetailResponse.from(member));
+
+        //TODO : CommentList 도 반환해야함.
+
     }
 }
