9ae4c0233768284d22b6b3c624ba6cecc0d27ee4
김도현 <nanayagoon@daum.net>
2024-08-27 23:08:25 +0900

    Feat/#11: 게시물 조회 예외처리 추가

--- src/main/java/com/example/board/domain/board/service/BoardService.java
+++ src/main/java/com/example/board/domain/board/service/BoardService.java
@@ -1,6 +1,8 @@
 package com.example.board.domain.board.service;
 
 import com.example.board.domain.board.entity.Board;
+import com.example.board.global.exception.BaseException;
+import com.example.board.global.exception.ErrorCode;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,9 +14,8 @@ public class BoardService {
 
     public Board findById(long id) {
         return boardRepository.findById(id).orElseThrow(
-                RuntimeException::new
+                () -> BaseException.from(ErrorCode.BOARD_NOT_FOUND)
         );
-        //TODO : 예외처리 작성하기!
     }
 
     public void save(Board board) {
