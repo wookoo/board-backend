632d6f99c921ee690bd87f9d8cb4fa07e6c3995d
김도현 <nanayagoon@daum.net>
2024-08-28 21:50:58 +0900

    Feat/#13: 댓글 작성 구현

--- src/main/java/com/example/board/application/controller/BoardController.java
+++ src/main/java/com/example/board/application/controller/BoardController.java
@@ -1,11 +1,14 @@
 package com.example.board.application.controller;
 
 import com.example.board.application.dto.request.BoardWriteRequest;
+import com.example.board.application.dto.request.CommentWriteRequest;
 import com.example.board.application.dto.response.BoardDetailResponse;
 import com.example.board.application.facade.BoardFacade;
+import com.example.board.application.facade.CommentFacade;
 import com.example.board.domain.board.service.BoardService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 public class BoardController {
 
     private final BoardFacade boardFacade;
+    private final CommentFacade commentFacade;
 
     @PostMapping()
     public ResponseEntity<Void> writeBoard(@RequestBody BoardWriteRequest boardWriteRequest) {
@@ -29,6 +33,13 @@ public class BoardController {
     public ResponseEntity<BoardDetailResponse> getBoard(@PathVariable(name = "id") long id) {
         BoardDetailResponse boardDetailResponse = boardFacade.getBoard(id);
         return ResponseEntity.ok(boardDetailResponse);
+    }
+
+    @PostMapping("/{id}/comment")
+    public ResponseEntity<Void> writeComment(@PathVariable(name = "id") long boardId, @RequestBody CommentWriteRequest commentWriteRequest){
+        String content = commentWriteRequest.getContent();
+        commentFacade.writeComment(boardId,content);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
 
--- src/main/java/com/example/board/domain/comment/entity/Comment.java
+++ src/main/java/com/example/board/domain/comment/entity/Comment.java
@@ -8,12 +8,14 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
+@EntityListeners(AuditingEntityListener.class)
 public class Comment {
 
     @Id
